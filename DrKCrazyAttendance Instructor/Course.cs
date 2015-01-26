402351d3970fb578235b2cae287a83e9babb5d41
Pickle15 <dill16pickle1993@gmail.com>
2015-01-25 19:36:20 -0500

    Added two new classes to handle Courses.

---
+++ DrKCrazyAttendance Instructor/Course.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrKCrazyAttendance_Instructor
+{
+    class Course
+    {
+        public Course(DayOfWeek[] days, string classroom, string courseName, string section,
+            string semester, DateTime startDateTime, DateTime endDateTime, TimeSpan gracePeriod,
+            bool logTardy)
+        {
+            this.Days = days;
+            this.ClassRoom = classroom;
+            this.CourseName = courseName;
+            this.Section = section;
+            this.Semester = semester;
+            this.StartDateTime = startDateTime;
+            this.EndDateTime = endDateTime;
+            this.GracePeriod = gracePeriod;
+            this.LogTardy = logTardy;
+        }
+
+        #region Properties
+        public DayOfWeek[] Days
+        {
+            get;
+            set;
+        }
+
+        public string ClassRoom
+        {
+            get;
+            set;
+        }
+
+        public string CourseName
+        {
+            get;
+            set;
+        }
+
+        public string Section
+        {
+            get;
+            set;
+        }
+
+        public string Semester
+        {
+            get;
+            set;
+        }
+
+        public DateTime StartDateTime
+        {
+            get;
+            set;
+        }
+
+        public DateTime EndDateTime
+        {
+            get;
+            set;
+        }
+
+        public TimeSpan GracePeriod
+        {
+            get;
+            set;
+        }
+
+        public bool LogTardy
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        public void Insert()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+    }
+}
---
+++ DrKCrazyAttendance Instructor/Courses.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrKCrazyAttendance_Instructor
+{
+    class Courses
+    {
+        public Courses(string classroom)
+        {
+            FetchAll(classroom);
+        }
+
+        #region properties
+        public List<Course> Courses
+        {
+            get;
+            private set;
+        }
+        #endregion
+        
+        #region methods
+        public List<Course> FetchAll(string classroom)
+        {
+            Courses = new List<Course>();
+            return Courses;
+        }
+
+        public void Add(Course course)
+        {
+            Courses.Add(course);
+        }
+
+        public void Remove(Course course)
+        {
+            Courses.Remove(course);
+        }
+
+        public void Update(Course course)
+        {
+
+        }
+
+        public Course GetCourse(string courseName, string section)
+        {
+            foreach (Course course in Courses)
+            {
+                if (course.CourseName.Equals(courseName) && course.Section.Equals(section))
+                {
+                    return course;
+                }
+            }
+            return null;
+        }
+        #endregion
+
+    }
+}
