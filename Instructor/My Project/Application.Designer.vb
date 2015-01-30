74937a59dde43c8c08cac822e4379c4b8c72823b
Pickle15 <dill16pickle1993@gmail.com>
2015-01-30 13:53:19 -0500

    Small fix with the intrsutor startup properties to start the correct form.

--- Instructor/My Project/Application.Designer.vb
+++ Instructor/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.DrKCrazyAttendance.Form1
+            Me.MainForm = Global.DrKCrazyAttendance.AttendanceServer
         End Sub
     End Class
 End Namespace
