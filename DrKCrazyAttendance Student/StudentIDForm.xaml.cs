f49d475e1d55ef6db680af7f377ab21495f1ce99
Pickle15 <dill16pickle1993@gmail.com>
2015-02-13 15:18:33 -0500

    Added student id form

---
+++ DrKCrazyAttendance Student/StudentIDForm.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DrKCrazyAttendance_Student
+{
+    /// <summary>
+    /// Interaction logic for StudentIDForm.xaml
+    /// </summary>
+    public partial class StudentIDForm : Window
+    {
+        public StudentIDForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtConID2.Clear();
+            txtConID3.Clear();
+            txtstuID2.Clear();
+            txtstuID3.Clear();
+        }
+    }
+}
