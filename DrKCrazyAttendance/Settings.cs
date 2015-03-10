0195d0aef63a5a1b85f08ab5950b64d5b3f1e184
Pickle15 <dill16pickle1993@gmail.com>
2015-03-10 19:53:12 -0400

    Further attempts to get encryption working -added missing files

---
+++ DrKCrazyAttendance/Settings.cs
@@ -0,0 +1,47 @@
+﻿using DrKCrazyAttendance.Util;
+using System;
+using System.Text;
+namespace DrKCrazyAttendance.Properties {
+    
+    
+    // This class allows you to handle specific events on the settings class:
+    //  The SettingChanging event is raised before a setting's value is changed.
+    //  The PropertyChanged event is raised after a setting's value is changed.
+    //  The SettingsLoaded event is raised after the setting values are loaded.
+    //  The SettingsSaving event is raised before the setting values are saved.
+    public sealed partial class Settings {
+        
+        public Settings() {
+            this.SettingsSaving += SettingsSavingEventHandler;
+            this.SettingsLoaded += SettingsLoadingEventHandler;
+        }
+
+        //Decrypts settings from config file
+        private void SettingsLoadingEventHandler(object sender, System.Configuration.SettingsLoadedEventArgs e)
+        {
+            if (Settings.Default.Encrypted)
+            {
+                try
+                {
+                    Settings.Default.SqlDatabase = SecurityCrypt.AES_Decrypt(Settings.Default.SqlDatabase);
+                    Settings.Default.SqlServerAddr = SecurityCrypt.AES_Decrypt(Settings.Default.SqlServerAddr);
+                    Settings.Default.SqlUsername = SecurityCrypt.AES_Decrypt(Settings.Default.SqlUsername);
+                    Settings.Default.SqlPassword = SecurityCrypt.AES_Decrypt(Settings.Default.SqlPassword);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+        
+        //Encrypts before being written to a file
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
+            Settings.Default.SqlDatabase = SecurityCrypt.AES_Encrypt(Settings.Default.SqlDatabase);
+            Settings.Default.SqlServerAddr = SecurityCrypt.AES_Encrypt(Settings.Default.SqlServerAddr);
+            Settings.Default.SqlUsername = SecurityCrypt.AES_Encrypt(Settings.Default.SqlUsername);
+            Settings.Default.SqlPassword = SecurityCrypt.AES_Encrypt(Settings.Default.SqlPassword);
+            Settings.Default.Encrypted = true;
+        }
+    }
+}
