d25e73272c30f072cfc9a3ef0616c7d6cbfcfcdd
srgillis <samuelrgillis@gmail.com>
2015-03-11 04:51:16 -0400

    Validates and deletes student from form "needs sql command to delete from db"

--- DrKCrazyAttendance Instructor/AttendanceReport.xaml.cs
+++ DrKCrazyAttendance Instructor/AttendanceReport.xaml.cs
@@ -106,10 +106,15 @@ namespace DrKCrazyAttendance_Instructor
 
         //datagrid delete button event
         private void Delete_Button_Click(object sender, RoutedEventArgs e)
-        {
+            
             //Attendance obj = ((FrameworkElement)sender).DataContext as Attendance;
+             {var  result = MessageBox.Show("Are you sure you want to delete this student?", "Validation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if ( result == MessageBoxResult.Yes && attendanceDataGrid.SelectedItem != null)
+            {
+                attendanceDataGrid.Items.Remove(attendanceDataGrid.SelectedItem);
+            }
         }
-
+        
         //Save this object into a CSV file
         public void SaveToFile(Stream stream)
         {
