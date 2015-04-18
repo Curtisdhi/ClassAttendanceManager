5e0d7e8a5b4f500e1f3f063a97648041fe467524
Pickle15 <dill16pickle1993@gmail.com>
2015-04-18 18:32:30 -0400

    Fixed crash when config is unencrypted

--- DrKCrazyAttendance Instructor/App.xaml.cs
+++ DrKCrazyAttendance Instructor/App.xaml.cs
@@ -15,9 +15,5 @@ namespace DrKCrazyAttendance_Instructor
     public partial class App : Application
     {
 
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            Settings.Default.Instructor = Environment.UserName;
-        }
     }
 }
